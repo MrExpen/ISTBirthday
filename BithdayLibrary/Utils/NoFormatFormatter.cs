c71b854d7a42e8af44b0f485919537a6bdbff4c4
MrExpen <chi-dimochka@yandex.ru>
2021-11-03 01:27:11 +0400

    + класс заглушка в случае ненадобности форматирования

---
+++ BithdayLibrary/Utils/NoFormatFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayLibrary.Utils
+{
+    public class NoFormatFormatter : BaseServiceTextFormatter, IServiceTextFormatter
+    {
+        public override string Format(string text, Format format, string linkText = null, string language = null)
+            => text;
+    }
+}
